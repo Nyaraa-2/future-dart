6b1b18d4f9ee515f1ccb489fbd6a94b0933aaffb
Nyaraa-2 <marina.vandenbosch@outlook.fr>
2022-09-07 15:40:23 +0200

    add error

--- lib/service/agifyService.dart
+++ lib/service/agifyService.dart
@@ -2,12 +2,11 @@ import 'package:api_dart/model/person.dart';
 import 'package:dio/dio.dart';
 
 const String api = "https://api.agify.io";
-Future<dynamic> getPerson({required String name}) async {
+Future<Person> getPerson({required String name}) async {
   try {
     var response = await Dio().get(api, queryParameters: {"name": name});
     return Person.fromJson(response.data);
   } catch (e) {
-    print(e);
-    return;
+    throw Error();
   }
 }
